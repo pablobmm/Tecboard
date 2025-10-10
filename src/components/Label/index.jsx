e93e81c766598a413dd0c580754be4063e187fdf
Pablo <148821043+pablobmm@users.noreply.github.com>
2025-10-10 17:11:23 -0300

    feat:Label estilizado

--- src/components/Label/index.jsx
+++ src/components/Label/index.jsx
@@ -1,6 +1,7 @@
+import './label.estilos.css'
 export function Label({ children, htmlFor }) {
     return (
-        <label htmlFor={htmlFor}>
+        <label htmlFor={htmlFor} className='label'>
             {children}
         </label>
     )
