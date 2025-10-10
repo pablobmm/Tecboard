1ef9c9f2dbf9f7be8e6607ec9ec73f6e2abb62c5
Pablo <148821043+pablobmm@users.noreply.github.com>
2025-10-10 18:47:31 -0300

    feat:Adicionado a data do evento

--- src/components/FormularioEvento/index.jsx
+++ src/components/FormularioEvento/index.jsx
@@ -1,24 +1,37 @@
+import './formulario-de-evento.estilos.css'
 import { CampoDeEntrada } from "../CampoDeEntrada";
 import { CampoDeFormulario } from "../CampoDeFormulario";
 import { Label } from "../Label";
 import { TituloFormulario } from "../TituloFormulario";
 
-export function FormularioDeEvento(){
-  return(
+export function FormularioDeEvento() {
+  return (
     <form className='form-evento'>
-        <TituloFormulario >
-          Preencha para criar um evento:
-        </TituloFormulario>
-      <CampoDeFormulario>
-        <Label htmlFor='nome'>
-          Qual o nome do evento ?
-        </Label>
-        <CampoDeEntrada 
-        type='text'
-        id='nome'
-        placeholder='Summer dev hits'
-        />
-      </CampoDeFormulario>
+      <TituloFormulario >
+        Preencha para criar um evento:
+      </TituloFormulario>
+      <div className="campos">
+        <CampoDeFormulario>
+          <Label htmlFor='nomeEvento'>
+            Qual o nome do evento ?
+          </Label>
+          <CampoDeEntrada
+            type='text'
+            id='nomeEvento'
+            placeholder='Summer dev hits'
+          />
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor='nomeEvento'>
+            Data do Evento
+          </Label>
+          <CampoDeEntrada
+            type='date'
+            id='dataEvento'
+            placeholder='dataEvento'
+          />
+        </CampoDeFormulario>
+      </div>
     </form>
   )
 }
