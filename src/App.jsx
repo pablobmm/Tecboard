94926a2676fd179cdeb7b96ff02b51ba0c4afae1
Pablo <148821043+pablobmm@users.noreply.github.com>
2025-10-11 00:59:32 -0300

    feat: Adiciona função map e key para renderizar itens

--- src/App.jsx
+++ src/App.jsx
@@ -3,58 +3,67 @@ import { Banner } from './components/Banner/index.jsx'
 import { FormularioDeEvento } from './components/FormularioEvento/index.jsx'
 import { Tema } from './components/Tema/index.jsx'
 
-const temas = [
-  {
-    id:1,
-    nome:'front-end'
-  },
-  {
-    id:2,
-    nome:'back-end'
-  },
-  {
-    id:3,
-    nome:'devops'
-  },
-  {
-    id:4,
-    nome:'inteligencia artificial'
-  },
-  {
-    id:5,
-    nome:'data science'
-  },
-  {
-    id:6,
-    nome:'cloud'
-  }
-]
+
 function App() {
+
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligencia artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    }
+  ]
+
+
   return (
     <main>
       <header>
-        <img src='/logo.png'alt=''/>
+        <img src='/logo.png' alt='' />
       </header>
-      <Banner/>
+      <Banner />
       <FormularioDeEvento />
-      <section>
-        <Tema tema={temas[0]}/>
+      {temas.map(function (item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item} />
+          </section>
+        )
+      })}
+
+      {/*<section>
+        <Tema tema={temas[1]} />
       </section>
       <section>
-        <Tema tema={temas[1]}/>
+        <Tema tema={temas[2]} />
       </section>
       <section>
-        <Tema tema={temas[2]}/>
+        <Tema tema={temas[3]} />
       </section>
       <section>
-        <Tema tema={temas[3]}/>
+        <Tema tema={temas[4]} />
       </section>
       <section>
-        <Tema tema={temas[4]}/>
-      </section>
-      <section>
-        <Tema tema={temas[5]}/>
-      </section>
+        <Tema tema={temas[5]} />
+      </section>*/}
     </main>
   )
 }
--- src/components/CampoDeEntrada/index.jsx
+++ src/components/CampoDeEntrada/index.jsx
@@ -1,5 +1,5 @@
 import './campo-entrada.estilos.css'
-export function CampoDeEntrada(...props){
+export function CampoDeEntrada(props){
     return(
         <input {...props} className='campo-entrada-form'/>
     )
